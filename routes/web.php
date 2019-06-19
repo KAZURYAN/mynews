0bd587b77a221445e6901780e6e9802af0a9ab09
”mochida” <kazunori.m.1986@gmail.com>
2019-06-19 12:50:37 +0000

    13章 20190619課題提出@mochida

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,13 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin'], function(){
   Route::get('news/create' , 'Admin\NewsController@add')->middleware('auth');
+  Route::post('news/create' , 'Admin\NewsController@create')->middleware('auth');;
   
-  /* 課題4 */
   Route::get('profile/create' , 'Admin\ProfileController@add')->middleware('auth');
+  Route::post('profile/create' , 'Admin\ProfileController@create')->middleware('auth');
   Route::get('profile/edit'   , 'Admin\ProfileController@edit')->middleware('auth');
+  Route::post('profile/edit' , 'Admin\ProfileController@update')->middleware('auth');
 });
-
-/* 課題3 */
-// Route::get('XXX', 'Admin\AAAController@bbb');
 
 Auth::routes();
 
